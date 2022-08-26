39d3ae9f09d39216a56dc0a6dfb1f76975ce229c
maks <mamonov.maksim.20003@gmail.com>
2022-08-26 17:56:55 +0500

    создал геттеры и сеттеры

--- src/Book.java
+++ src/Book.java
@@ -1,6 +1,38 @@
 public class Book {
-    String name;
-    Auther author;
-    int year;
+    private static String name;
+    private Author author;
+    private int year;
 
+
+
+    public Book(String name, Author author, int year) {
+        this.name = name;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Author getAuthor() {
+        return this.author;
+    }
+
+    public int grtYear() {
+        return  this.year;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public void  setYear(int year) {
+        this.year = year;
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+    public static Book editingABook(String name, Author author, int year) {
+        Book book = new Book();
+        String book1 = Book.name;
+        Author.author =
+
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
 }
